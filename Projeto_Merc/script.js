e55958677a7bbc06a03b7f11e99c93183353be29
r1cardo360 <ric.furlanetto@hotmail.com>
2024-09-26 22:30:23 -0300

    Adicionando banco de dados postgres e configurando

--- Projeto_Merc/script.js
+++ Projeto_Merc/script.js
@@ -4,6 +4,33 @@ function menuOnClick() {
     document.getElementById("menu-bg").classList.toggle("change-bg");
   }
 
+  function enviarUsuario(){
+    document.getElementById("cadastroUsuario").addEventListener('submit', async (event) => {
+        event.preventDefault();
+
+        const nome_usuario = document.getElementById("nome_usuario").value;
+
+        const response = await fetch('/usuarios',{
+            method: "post",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({nome_usuario})
+        });
+
+        document.getElementById("nome_usuario").value = '';
+
+        if(response.ok){
+            const data = await response.json();
+            alert(data.message);
+        }else{
+            const errorData = await response.json();
+            alert(errorData.error);
+        }
+
+    })
+  }
+
 function loadComponent(id, file){
     fetch(file)
         .then(response => response.text())
